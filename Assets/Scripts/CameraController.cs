e9f52d9f3198c9b530a06c20396a94662e81e62e
Darina Nguyen <locovna@gmail.com>
2021-12-05 10:33:01 +0200

    Implement camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 10f;
+    public float minY = 5f;
+    public float maxY = 20f;
+
+    void Update()
+    {
+        // store current camera position
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) 
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) 
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) 
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) 
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        // Mathf.Clamp(value to limit, limit range) 
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minY, maxY);
+
+        transform.position = pos;
+    }
+}
