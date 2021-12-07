fe558d641d564ff3f69c26b1d4a73b63f92b1179
Darina Nguyen <locovna@gmail.com>
2021-12-07 16:21:43 +0200

    Add EditorCheats

---
+++ Assets/Editor/EditorCheats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class EditorCheats : MonoBehaviour
+{
+    [MenuItem("Cheats/Reload Current Scene")]
+    public static void ReloadCurrentScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        Debug.Log(scene.name + " has reloaded");
+    }
+}
