3cd02f65821f16fc5a0ea754235be7f190acf133
Darina Nguyen <locovna@gmail.com>
2021-12-07 11:16:23 +0200

    Implement health bar

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterHealth : MonoBehaviour
+{
+    public Image healthBar;
+    public float healthLimit = 100;
+    public float health = 100;
+
+    void Update()
+    {
+        if(health <= 0)
+        {
+            // restart application
+            // todo: use SceneManager.LoadScene
+            // Application.LoadLevel(Application.loadedLevel);
+        }
+
+        // todo: remove the example
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(20);
+            UpdateHealthBar();
+        }
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Healing(10);
+            UpdateHealthBar();
+        }
+    }
+
+    public void TakeDamage(float damagePoints) 
+    {
+        health -= damagePoints;
+    }
+
+    public void Healing(float healPoints)
+    {
+        health += healPoints;
+        health = Mathf.Clamp(health, 0, healthLimit);
+    }
+
+    // todo: separate UI logic
+    // fix: reference error
+    public void UpdateHealthBar()
+    {
+        healthBar.fillAmount = health / healthLimit;
+    }    
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Character : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
     public LayerMask whatCanBeClickedOn;
     private NavMeshAgent myAgent;
