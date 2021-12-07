a6b75bfa24ff5857c973bad5475df8682bee4bf1
Darina Nguyen <locovna@gmail.com>
2021-12-07 17:22:26 +0200

    Implement health bar facing camera functionality

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform cam;
+    private float initialRotation;
+
+    void Start()
+    {
+        initialRotation = transform.rotation.z;
+    }
+
+    // called after Update() so Camera has already changed the position
+    void LateUpdate()
+    {
+        //transform.LookAt(transform.position + cam.position);
+        transform.rotation = cam.rotation;
+        transform.Rotate(Vector3.up * initialRotation * 180); // fix
+    }
+}
