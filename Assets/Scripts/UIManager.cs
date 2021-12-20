0d314ead3e3c7848c79c26d39984817c62d21cb0
Darina Nguyen <locovna@gmail.com>
2021-12-20 13:28:28 +0200

    Implement static top bar

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public bool cheatMode = true;
+    public GameObject MainBar;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(cheatMode)
+            MainBar.SetActive(true);
+        else
+            MainBar.SetActive(false);
+    }
+}
