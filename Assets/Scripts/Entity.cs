89c4517940822b82728d1cf049b8d4a4da0094d2
Darina Nguyen <locovna@gmail.com>
2021-12-29 19:24:18 +0200

    Update Entity class

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,33 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class Entity
+namespace Home
 {
-    public int id;
-    public float healthLimit;
-    public float health;
-    public float baseDamage;
-    public string name;
-    public Vector3 coordinates;
+    public class Entity
+    {
+        public string id { get; protected set; }
+        public float healthLimit { get; set; }
+        public float selfDamage { get; set; }
+        public string name { get; set; }
+        private float _health;
 
-    public Entity(float healthLimit, float health, float baseDamage, string name)
-    {
-        this.id = Random.Range(0,1000);
-        this.healthLimit = healthLimit;
-        this.health = health; // make check if health <= health limit
-        this.baseDamage = baseDamage;
-        this.name = name;
-    }
+        public float health
+        {
+            get { return _health; }
+            set
+            {
+                if (value <= healthLimit)
+                    _health = value;
+            }
+        }
 
-    public void Heal(float healingPoints)
-    {
-        this.health += healingPoints;
-    }
-    
-    public void TakeDamage(float damagePoints)
-    {
-        this.health -= damagePoints;
+        public Entity(float healthLimit, float health, float selfDamage, string name)
+        {
+            this.id = Helper.GenerateGUID();
+            this.healthLimit = healthLimit;
+            this.health = health;
+            this.selfDamage = selfDamage;
+            this.name = name;
+        }
+
+        public void Heal(float healingPoints)
+        {
+            this.health += healingPoints;
+        }
+
+        public void TakeDamage(float damagePoints)
+        {
+            this.health -= damagePoints;
+        }
     }
 }
 
@@ -39,6 +52,6 @@ class Example
         var testEntity = new Entity(100f, 100f, 10f, "Prikolchik");
         testEntity.Heal(50);
         testEntity.TakeDamage(5);
-        Debug.Log($"Hey, {testEntity.name} #{testEntity.id} your health is {testEntity.health}, but base damage is {testEntity.baseDamage}.");
+        Debug.Log($"Hey, {testEntity.name} #{testEntity.id} your health is {testEntity.health}, but base damage is {testEntity.selfDamage}.");
     }
 }
