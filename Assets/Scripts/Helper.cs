8ae510095f668a444e16abac8dfd6aa60d438040
Darina Nguyen <locovna@gmail.com>
2021-12-29 19:25:36 +0200

    Add Helper class

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Home
+{
+    public class Helper
+    {
+        static public string GenerateGUID()
+        {
+            System.Guid guid = System.Guid.NewGuid();
+            return guid.ToString();
+        }
+    }
+}
+
