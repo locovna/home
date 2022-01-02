14a06a7e02595fa8525809305ede455386110396
Darina Nguyen <locovna@gmail.com>
2022-01-02 21:26:29 +0200

    Add MovementController

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Home
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+
+    public class MovementController : MonoBehaviour
+    {
+        public LayerMask whatCanBeClickedOn;
+        private NavMeshAgent agentCharacter;
+
+        void Start()
+        {
+            agentCharacter = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            MoveToClick();
+        }
+
+        private void MoveToClick()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo, 100, whatCanBeClickedOn))
+                {
+                    agentCharacter.SetDestination(hitInfo.point);
+                }
+            }
+        }
+    }
+}
