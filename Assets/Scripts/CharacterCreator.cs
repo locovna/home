53eece8bc1005b57c46b1759c5c0c20a76885f3f
Darina Nguyen <locovna@gmail.com>
2022-01-08 00:02:20 +0200

    Add Character Creator

---
+++ Assets/Scripts/CharacterCreator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Home
+{
+    static public class CharacterCreator
+    {
+        static public Character CreateCharacter()
+        {
+            Character createdCharacter = new Character();
+            return createdCharacter;
+        }
+    }
+}
