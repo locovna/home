7039eca835ddeb61c319442a053a335483d5419a
Darina Nguyen <locovna@gmail.com>
2022-01-08 00:02:49 +0200

    Add Game Manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Home
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int numberOfCharacters = 10;
+        public List<Character> characterList = new List<Character>();
+
+        void Start()
+        {
+            CreateCharacters(numberOfCharacters);
+            print(characterList.Count);
+        }
+
+        public void CreateCharacters(int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                Character character = CharacterCreator.CreateCharacter();
+                characterList.Add(character);
+            }
+        }
+    }
+}
