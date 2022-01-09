770ae21e623687026aa570ac6e28e165682895d9
Darina Nguyen <locovna@gmail.com>
2022-01-09 20:33:45 +0200

    Add CharacterBehaviour

---
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Home
+{
+    public class CharacterBehaviour : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+        private Character character;
+
+        public void InitializeCharacter(Character characterToInitialize)
+        {
+            character = characterToInitialize;
+            SetName();
+        }
+
+        private void SetName()
+        {
+            text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = character.name;
+            print(gameObject.GetInstanceID() + ": data in character behaviour: " + character.name);
+        }
+    }
+}
