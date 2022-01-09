c2c0b2796b28ed3e595909f9e8759a45249382e1
Darina Nguyen <locovna@gmail.com>
2022-01-09 20:32:15 +0200

    Add CreateRandomizedCharacter method

--- Assets/Scripts/CharacterCreator.cs
+++ Assets/Scripts/CharacterCreator.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Home
 {
     static public class CharacterCreator
     {
-        static public Character CreateCharacter()
+        static private string[] RandomNames =
+                        {"Prikolchik","Loshok",
+                        "Pips","Chelik",
+                        "Caratel","Ulyot",
+                        "Chaika","Loh"};
+
+        static public Character CreateRandomizedCharacter()
         {
-            Character createdCharacter = new Character();
+            string name = RandomNames[Random.Range(0, RandomNames.Length - 1)];
+            Character createdCharacter = new Character(100f, 100f, 10f, name, 10f);
+            Debug.Log(nameof(CharacterCreator) + " name: " + createdCharacter.name);
             return createdCharacter;
         }
     }
