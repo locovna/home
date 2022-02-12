39f13f50218b4aac12b0fe1d08ef0246d0e029af
Darina Nguyen <locovna@gmail.com>
2022-02-12 13:11:22 +0200

    Higlight resources on mouse enter

--- Assets/Scripts/GameCore/ResourceBehaviour.cs
+++ Assets/Scripts/GameCore/ResourceBehaviour.cs
@@ -6,7 +6,51 @@ namespace Home
 {
     public class ResourceBehaviour : MonoBehaviour
     {
-        public Resource resource; // private
+        public Resource resource;
+        public bool active = false;
+
+        public Texture2D cursorTexture;
+        public CursorMode cursorMode = CursorMode.ForceSoftware;
+        public Vector2 hotSpot = Vector2.zero;
+
+        Color colorHower = Color.red;
+        Color colorClicked = Color.green;
+        Color colorInitial;
+        Renderer rendererComponent;
+
+        void Start()
+        {
+            rendererComponent = GetComponent<Renderer>();
+            colorInitial = rendererComponent.material.GetColor("_Color");
+        }
+
+        void OnMouseEnter()
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+
+        void OnMouseExit()
+        {
+            Cursor.SetCursor(null, hotSpot, cursorMode);
+            if (active == false)
+            {
+                rendererComponent.material.color = colorInitial;
+            }
+        }
+
+        void OnMouseDown()
+        {
+            if (active == false)
+            {
+                active = true;
+                rendererComponent.material.color = colorClicked;
+            }
+            else
+            {
+                active = false;
+                rendererComponent.material.color = colorInitial;
+            }
+        }
 
         public void InitializeResource(Resource resourceToInitialize)
         {
